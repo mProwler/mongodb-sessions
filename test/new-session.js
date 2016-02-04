ee1d933cc6347bb965c4349f1a8a4a32fb2670cd
Joshua Ecklund <joshua.ecklund@charter.com>
2016-02-04 13:04:01 -0700

    Test session - Step 1

--- test/new-session.js
+++ test/new-session.js
@@ -1,12 +1,20 @@
-var request = require('supertest')(require('../app'))
-var assert = require('chai').assert
+var request = require('supertest')(require('../app'));
+var assert = require('chai').assert;
+var cheerio = require('cheerio');
 
 describe('the home page', function () {
   it('should not set a cookie before login', function (done) {
-
-  })
+    request.get('/').expect(200).end(function(err, res) {
+      assert.notProperty(res.headers, 'set-cookie');
+      done();
+    });
+  });
 
   it('should show login form', function (done) {
-
-  })
-})
+    request.get('/').expect(200).end(function (err, res) {
+      var $ = cheerio.load(res.text);
+      assert.equal($('div.login').length, 1);
+      done();
+    });
+  });
+});
